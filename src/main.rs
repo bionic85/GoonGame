ea4474f597991711e4ef4e1de3b4a3b3fe5e0076
bionic85 <144353436+bionic85@users.noreply.github.com>
2026-02-02 22:46:32 +0100

    lil push

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod level1;
+mod player;
 
 use bevy::prelude::*;
+use player::*;
 
 fn main() {
+    let player = Player {pos: Vec3::ZERO, goon_points: 0, goon_speed: 0.0};
     // crazy builder pattern taaaaa
     App::new().add_plugins(DefaultPlugins).add_plugins(level1::Level1).run();
 }
---
+++ src/player.rs
@@ -0,0 +1,22 @@
+﻿use bevy::prelude::*;
+
+pub struct Player {
+    pub(crate) pos: Vec3,
+    pub(crate) goon_points: i32,
+    pub(crate) goon_speed: f32,
+}
+
+pub fn player_move(mut player: Player) -> Player {
+    player.pos = Vec3::ZERO;
+    player
+}
+
+fn calculate_goon_stage(player: Player) -> i32 {
+    return 0; //TODO: implement goon stages so that the player model grows
+}
+
+// fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+//     for mut transform in &mut query {
+//         transform.rotate_y(time.delta_secs() / 2.);
+//     }
+// }
